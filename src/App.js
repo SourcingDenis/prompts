d797a2e6fe9756f056290d269d5afda177ff5de0
Denys Dinkevych <den.dinkevych.edu@gmail.com>
2023-04-11 07:57:28 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import PromptList from './components/PromptList';
+import SearchBar from './components/SearchBar';
+import CategoryFilter from './components/CategoryFilter';
+import SubmitPrompt from './components/SubmitPrompt';
 import './App.css';
 
 function App() {
+  const [prompts, setPrompts] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [selectedCategory, setSelectedCategory] = useState('');
+
+  useEffect(() => {
+    fetchPrompts();
+  }, []);
+
+  const fetchPrompts = async () => {
+    const response = await axios.get('GITHUB_RAW_CSV_URL');
+    const fetchedPrompts = parseCSV(response.data);
+    setPrompts(fetchedPrompts);
+  };
+
+  const parseCSV = (data) => {
+    // Implement CSV parsing logic
+  };
+
+  const filteredPrompts = prompts.filter((prompt) =>
+    prompt.text.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Prompt Sharing App</h1>
+      <SearchBar setSearchTerm={setSearchTerm} />
+      <CategoryFilter setSelectedCategory={setSelectedCategory} />
+      <PromptList prompts={filteredPrompts} />
+      <SubmitPrompt />
     </div>
   );
 }
